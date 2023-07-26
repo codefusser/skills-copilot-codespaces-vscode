b9f6004fb6c6fbba1fbd3343189e3e2ad96bf16b
Kingsley Uchunor <kizucom@gmail.com>
2023-07-26 02:19:28 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember(){
+    return {
+        Name: "Kingsley Uchunor",
+        Date: "07/27/2023",
+        Description: "Copilot programming companion",
+        Version: "0.0.1"
+    }
+}
